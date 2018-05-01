0923a73fd9386c8f3cec32a99d078c3c22d95a3a
Jonathan B <andomorphia@gmail.com>
2018-05-01 14:55:00 +0200

    test local axios request to database with data in local state

--- client/src/components/deck/DeckSingle.js
+++ client/src/components/deck/DeckSingle.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
 import DeckMeta from './DeckMeta';
 import DeckRating from './DeckRating';
@@ -6,22 +7,47 @@ import DeckList from './DeckList';
 import DeckDesc from './DeckDesc';
 import DeckComments from './comments/DeckComments';
 
-const DeckSingle = () => (
-  <main>
-    <section className="section" id="deck-single">
-      <div className="container">
-        <div className="deck">
-          <div className="columns">
-            <DeckMeta />
-            <DeckRating />
+class DeckSingle extends Component {
+  constructor() {
+    super();
+    this.state = {
+      deck: {},
+    };
+  }
+
+  componentDidMount() {
+    this.getDeck('5ae1ef6d40f923503e4ed403');
+  }
+
+  // Get deck by id
+  getDeck = (id) => {
+    axios.get(`http://localhost:5000/api/decks/id/${id}`)
+    .then(res => {
+      this.setState({
+        deck: res.data
+      })
+    });
+  }
+
+  render() {
+    return (
+      <main>
+        <section className="section" id="deck-single">
+          <div className="container">
+            <div className="deck">
+              <div className="columns">
+                <DeckMeta />
+                <DeckRating />
+              </div>
+              <DeckList />
+              <DeckDesc />
+              <DeckComments />
+            </div>
           </div>
-          <DeckList />
-          <DeckDesc />
-          <DeckComments />
-        </div>
-      </div>
-    </section>
-  </main>
-);
+        </section>
+      </main>
+    );
+  }
+}
 
 export default DeckSingle;

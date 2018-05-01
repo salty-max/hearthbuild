7e9586a9b418b7d2035b5a89adf648d25538d7da
Jonathan B <andomorphia@gmail.com>
2018-05-01 14:50:27 +0200

    enable CORS server-side

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const cors = require('cors');
 
 const app = express();
 
 const users = require('./routes/api/users');
 const decks = require('./routes/api/decks');
+
+// Enable CORS
+app.use(cors());
 
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: true }));

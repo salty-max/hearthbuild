aaaa28242039eff74be410de05ca002b7d1d2229
Jonathan B <andomorphia@gmail.com>
2018-05-06 16:10:45 +0200

    fixed wrong prop call for deck description

--- client/src/components/deck/DeckDesc.js
+++ client/src/components/deck/DeckDesc.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const DeckDesc = ({ desc }) => (
   <div className="deck--desc">
     <div className="content">
       <h3>How to play this deck</h3>
-      <p>{desc.description}</p>
+      <p>{desc}</p>
     </div>
   </div>
 );
+
+DeckDesc.propTypes = {
+  desc: PropTypes.string.isRequired,
+};
 
 export default DeckDesc;

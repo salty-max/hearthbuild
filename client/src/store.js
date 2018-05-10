750a3a3ed80ce5ef851a7b4749fa422235588179
max-jellycat <max@jellycat.fr>
2018-05-10 22:40:15 +0200

    trying to fix redux devtools error

--- client/src/store.js
+++ client/src/store.js
@@ -1,4 +1,5 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 
 import rootReducer from './reducers';
@@ -10,9 +11,8 @@ const middlewares = [thunk, ajax];
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
+  composeWithDevTools(
     applyMiddleware(...middlewares),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 

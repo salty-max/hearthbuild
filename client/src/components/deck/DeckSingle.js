b5fba23925efc0450bf61cd597f21bd73a1c7d17
Maxime Blanc <max@jellycat.fr>
2018-05-10 17:56:17 +0200

    mana curve in deck page done

--- client/src/components/deck/DeckSingle.js
+++ client/src/components/deck/DeckSingle.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Chart, Bars, Layer, Ticks } from 'rumble-charts';
 
 import DeckMeta from './DeckMeta';
 import DeckRating from './DeckRating';
@@ -38,6 +39,13 @@ class DeckSingle extends Component {
 
     const deck = decks.find(deck => deck._id === deckId);
 
+    // Chart
+    const chartData = [{ data: [] }];
+
+    for (let i = 0; i <= 10; i++) {
+      chartData[0].data.push([i, deck.cards.filter(card => card.cost === i).length]);
+    }
+
     return (
       <main>
         <section className="section" id="deck-single">
@@ -56,7 +64,34 @@ class DeckSingle extends Component {
                 />
               </div>
               <DeckList cards={deck.cards} hsClass={deck.class} />
-              <DeckDesc desc={deck.description} />
+              <div className="columns">
+                <div className="column is-8">
+                  {deck.description === '' ? (
+                    <DeckDesc desc="The author did not leave any recommendations. Do or do not, there is no try !" />
+                  ) : (
+                    <DeckDesc desc={deck.description} />
+                  )}
+                </div>
+                <div className="column is-4">
+                  <div className="box mana-curve">
+                    <Chart width={400} height={400} series={chartData}>
+                      <Layer width="100%" height="80%" position="middle center">
+                        <Ticks
+                          axis='x'
+                          label={({index, props}) => props.series[0].data[0]}
+                          labelStyle={{ textAnchor: 'middle', alignmentBaseline: 'before-edge', fontSize: '1.2em', fill: '#383838' }}
+                          labelAttributes={{ y: 20 }}
+                        />
+                        <Bars
+                          colors={['#1EC2A7']}
+                          barWidth="9%"
+                          innerPadding="2%"
+                        />
+                      </Layer>
+                    </Chart>
+                  </div>
+                </div>
+              </div>
               <DeckComments
                 comments={deck.comments}
                 auth={auth}
